3fd267be5ae33d8bd8519934d7a5fd96115ca17a
tonio teran <teran@mit.edu>
2020-12-14 21:57:05 -0800

    Add empty gtsam utils file.

---
+++ include/tonioviz/GtsamUtils.h
@@ -0,0 +1,19 @@
+/**
+ * @file GtsamUtils.cpp
+ * @brief Utility functions for interacting with GTSAM objects.
+ * @author Tonio Teran, teran@mit.edu
+ * Copyright 2020 The Ambitious Folks of the MRG
+ */
+
+#ifndef TONIOVIZ_GTSAMUTILS_H_
+#define TONIOVIZ_GTSAMUTILS_H_
+
+namespace mrg {
+
+/**
+ * TODO(alan) Implement me.
+ */
+
+}  // namespace mrg
+
+#endif  // TONIOVIZ_GTSAMUTILS_H_
